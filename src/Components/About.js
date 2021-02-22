62640ab1631ee36fff05f1d8044ac240843b5ba4
Piotr Dudek <piotr72@gmail.com>
2021-02-22 00:58:36 -0500

    updated projects

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,13 +8,13 @@ class About extends Component {
          var name = this.props.data.name;
          // var profilepic= "images/"+this.props.data.image;
          var bio = this.props.data.bio;
-         var street = this.props.data.address.street;
+         // var street = this.props.data.address.street;
          var city = this.props.data.address.city;
          var state = this.props.data.address.state;
-         var zip = this.props.data.address.zip;
+         // var zip = this.props.data.address.zip;
          var phone = this.props.data.phone;
          var email = this.props.data.email;
-         var resumeDownload = this.props.data.resumedownload;
+         // var resumeDownload = this.props.data.resumedownload;
       }
 
       return (
@@ -37,11 +37,11 @@ class About extends Component {
                            <span>{city} {state}</span>
                         </p>
                      </div>
-                     <div className="columns download">
+                     {/* <div className="columns download">
                         <p>
                            <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Resume</a>
                         </p>
-                     </div>
+                     </div> */}
                   </div>
                </div>
             </div>
