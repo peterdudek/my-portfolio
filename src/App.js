4f7513678770f67786dc69ce8aaeda28f35ebf44
Piotr Dudek <piotr72@gmail.com>
2021-02-21 23:36:08 -0500

    updated education, cleaned style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
-import Contact from './Components/Contact';
 import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 import ResumeJSON from "./resumeData.json";
@@ -22,24 +21,10 @@ class App extends Component {
 
     ReactGA.initialize('UA-110570651-1');
     ReactGA.pageview(window.location.pathname);
-
   }
 
   getResumeData(){
-    // $.ajax({
-    //   url:'./resumeData.json',
-    //   dataType:'json',
-    //   cache: false,
-    //   success: function(data){
-
         this.setState({resumeData: ResumeJSON});
-
-    //   }.bind(this),
-    //   error: function(xhr, status, err){
-    //     console.log(err);
-    //     alert(err);
-    //   }
-    // });
   }
 
   componentDidMount(){
@@ -54,7 +39,6 @@ class App extends Component {
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
         <Testimonials data={this.state.resumeData.testimonials}/>
-        <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
